fdc33d35f261c7c4518cacb85fbc594787bbc5aa
sethcrist <sethcrist50@gmail.com>
2024-04-26 15:45:59 -0700

    fixed mocklists

--- src/mockList.js
+++ src/mockList.js
@@ -1,66 +1,58 @@
 let mockList = [
   {
     id: 1,
+    movie_api_id: 1096197,
     watched: true,
     rating: 1,
-    title: "No Way Up",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHo2RnxrY7bmO8C11SwzGICcUlU25a3HTExg&s",
     user_id: 1
   },
   {
     id: 2,
+    movie_api_id: 1041613,
     watched: false,
     rating: 0,
-    title: "Immaculate",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHo2RnxrY7bmO8C11SwzGICcUlU25a3HTExg&s",
     user_id: 1
   },
   {
     id: 3,
+    movie_api_id: 838209,
     watched: true,
     rating: 4,
-    title: "Exhuma",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHo2RnxrY7bmO8C11SwzGICcUlU25a3HTExg&s",
     user_id: 2
   },
   {
     id: 4,
+    movie_api_id: 1111873,
     watched: false,
     rating: 0,
-    title: "Abigail",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHo2RnxrY7bmO8C11SwzGICcUlU25a3HTExg&s",
     user_id: 2
   },
   {
     id: 5,
+    movie_api_id: 405774,
     watched: true,
     rating: 5,
-    title: "Imaginary",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHo2RnxrY7bmO8C11SwzGICcUlU25a3HTExg&s",
     user_id: 3
   },
   {
     id: 6,
+    movie_api_id: 1125311,
     watched: false,
     rating: 0,
-    title: "Bird Box",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHo2RnxrY7bmO8C11SwzGICcUlU25a3HTExg&s",
     user_id: 3
   },
   {
     id: 7,
+    movie_api_id: 760104,
     watched: true,
     rating: 5,
-    title: "X",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHo2RnxrY7bmO8C11SwzGICcUlU25a3HTExg&s",
     user_id: 4
   },
   {
     id: 8,
+    movie_api_id: 1127175,
     watched: false,
     rating: 0,
-    title: "Skal - Fight for Survival",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHo2RnxrY7bmO8C11SwzGICcUlU25a3HTExg&s",
     user_id: 4,
   }
 ]
