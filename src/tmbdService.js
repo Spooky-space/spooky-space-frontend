b5e8bdd011582d80af92ef4ca5f0d00574c2a928
sethcrist <sethcrist50@gmail.com>
2024-04-26 09:41:04 -0700

    moved the api key to the root

--- src/tmbdService.js
+++ src/tmbdService.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { apiConfig } from './apiConfig.env';
+import { apiConfig } from '../apiConfig.env';
 
 const useHorrorMovies = () => {
     const [movies, setMovies] = useState([]);
