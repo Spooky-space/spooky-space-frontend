ab9cb803a303bf5ae6e394701276ec6d608482c9
mark19242 <mark19242@gmail.com>
2024-04-29 13:21:16 -0700

    modals seem to be working, updating styling

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,47 +1,59 @@
-import React from "react"
+import React, { useState } from "react"
 import Image from "../assets/red-tree2.png"
 import Logo from "../assets/spooky-space-logo.png"
-import Modal from "../components/modal/Modal"
+import Modals from "../components/modal/Modal" // Ensure this import path is correct based on your project structure
 
-const handleClick = () => {
-	return <Modal />
-}
 const Landing = () => {
-	return (
-		<div className="page-body landing-background">
-			<div className="landing-background-image-container">
-				<img
-					src={Image}
-					alt="picture of glowing red tree in the dark"
-					className="landing-background-image"
-				/>
-			</div>
-			<div className="landing-content-container">
-				<div className="landing-logo-container">
-					<img
-						src={Logo}
-						alt="a redish full moon with the title 'Spooky Space on it'"
-						className="landing-logo"
-					/>
-				</div>
-				<div className="landing-button-container">
-					<button
-						className="create-account-button button"
-						onClick={handleClick}
-					>
-						Create Account
-					</button>
-					<button className="login-button button" onClick={handleClick}>
-						Log In
-					</button>
+  // State to control the visibility of the modal
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-					<button className="about-button button" onClick={handleClick}>
-						<Modal />
-					</button>
-				</div>
-			</div>
-		</div>
-	)
+  // Function to toggle the modal's visibility
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen)
+  }
+
+  return (
+    <div className="page-body landing-background">
+      <div className="landing-background-image-container">
+        <img
+          src={Image}
+          alt="picture of a glowing red tree in the dark"
+          className="landing-background-image"
+        />
+      </div>
+      <div className="landing-content-container">
+        <div className="landing-logo-container">
+          <img
+            src={Logo}
+            alt="a reddish full moon with the title 'Spooky Space' on it"
+            className="landing-logo"
+          />
+        </div>
+        <div className="landing-button-container">
+          <button
+            className="create-account-button button"
+            onClick={toggleModal} // This could be set for a different function if needed
+          >
+            Create Account
+          </button>
+          <button
+            className="login-button button"
+            onClick={toggleModal} // This could be set for a different function if needed
+          >
+            Log In
+          </button>
+          <button
+            className="about-button button"
+            onClick={toggleModal} // This button specifically toggles the modal
+          >
+            About Us
+          </button>
+        </div>
+      </div>
+      {/* Conditionally render the Modal based on the isModalOpen state */}
+      <Modals isOpen={isModalOpen} toggle={toggleModal} />
+    </div>
+  )
 }
 
 export default Landing
