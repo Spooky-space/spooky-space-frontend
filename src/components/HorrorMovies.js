aa8ca17527be5a4cd35149abd6b480f4d10898da
MattBarnabo <mattbarnabo@gmail.com>
2024-04-29 19:19:44 -0500

    adds Slider path to HorrorMovie component

--- src/components/HorrorMovies.js
+++ src/components/HorrorMovies.js
@@ -1,27 +1,35 @@
-import React from "react";
-import useHorrorMovies from "../tmdbService";
+import React from "react"
+import useHorrorMovies from "../tmdbService"
+import Slider from "./slider"
 
 const HorrorMovies = () => {
-  const { movies, isLoading, error } = useHorrorMovies()
-  const imageUrlBase = "https://image.tmdb.org/t/p/w500"
+	const { movies, isLoading, error } = useHorrorMovies()
+	const imageUrlBase = "https://image.tmdb.org/t/p/w500"
 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Error Loading Movies: {error.message}</div>
+	if (isLoading) return <div>Loading...</div>
+	if (error) return <div>Error Loading Movies: {error.message}</div>
 
-  return (
-    <div>
-      <h1>HorrorMovies</h1>
-      <ul>
-        {movies.map(movie => (
-          <li key={movie.id}>
-            <img src={imageUrlBase + movie.poster_path} alt={movie.title} />
-            <br />
-            {movie.title}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+	return (
+		<div>
+			<h1>HorrorMovies</h1>
+			<ul>
+				{movies.map((movie) => (
+					<li key={movie.id}>
+						<img src={imageUrlBase + movie.poster_path} alt={movie.title} />
+						<br />
+						{movie.title}
+					</li>
+				))}
+			</ul>
+			{movies.length > 0 && (
+				<Slider movies={movies}>
+					{movies.map((movie) => (
+						<Slider.Item movie={movie} key={movie.id}></Slider.Item>
+					))}
+				</Slider>
+			)}
+		</div>
+	)
 }
 
 export default HorrorMovies
