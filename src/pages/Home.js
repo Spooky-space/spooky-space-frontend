1976720a0e971abc3a67e4e7c8d0728c46240495
MattBarnabo <mattbarnabo@gmail.com>
2024-04-29 19:23:32 -0500

    adds slider to Home page and basic styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from "react"
 import HorrorMovies from "../components/HorrorMovies"
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Spooky Space Top Picks</h1>
-      <HorrorMovies />
-    </div>
-  )
+const Home = ({ movies }) => {
+	return (
+		<div className="page-body">
+			<div className="home-content">
+				<h1 className="home-header">Spooky Space Top Picks</h1>
+				<HorrorMovies />
+			</div>
+		</div>
+	)
 }
 
 export default Home
