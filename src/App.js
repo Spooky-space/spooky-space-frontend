688080e61b13b459c0d4edc199270482006003f1
MattBarnabo <mattbarnabo@gmail.com>
2024-04-29 19:17:01 -0500

    added routes, imported horror movie hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react"
-import HorrorMovies from "./components/HorrorMovies"
+import { Routes, Route } from "react-router-dom"
+import useHorrorMovies from "./components/HorrorMovies"
 import Landing from "./pages/Landing"
 import Home from "./pages/Home"
 import Show from "./pages/Show"
 import MyFlix from "./pages/MyFlix"
 import Index from "./pages/Index"
+import NotFound from "./pages/NotFound"
 import "./App.css"
 
 const App = () => {
-  return (
-    <>
-      {/* <Landing /> */}
-      <Home />
-    </>
-  )
+	const { movies } = useHorrorMovies()
+	console.log(movies)
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/home" element={<Home movies={movies} />} />
+				<Route path="/show" element={<Show />} />
+				<Route path="notFound" element={<NotFound />} />
+			</Routes>
+		</>
+	)
 }
 
 export default App
