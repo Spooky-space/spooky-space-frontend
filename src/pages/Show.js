9652527562c3543540be1962781e746a11e8aa09
sethcrist <sethcrist50@gmail.com>
2024-04-30 13:55:00 -0700

    added another variable to the base url to adjust the image size

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -4,6 +4,9 @@ import axios from "axios"
 import { apiConfig } from "../apiConfig"
 
 const Show = () => {
+  const imageUrlBase = "https://image.tmdb.org/t/p/original"
+  const smallImageUrlBase = "https://image.tmdb.org/t/p/w500"
+
   const { id } = useParams()
   const [movie, setMovie] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
@@ -37,9 +40,41 @@ return
 </div>
 
   return (
-    <div>
-      <h1>{movie.title}</h1>
-      {/* <p>{movie.over}</p> */}
+    <div className="show-body">
+      <div className="img-container">
+        <img src={imageUrlBase + movie.backdrop_path} 
+        alt={movie.title} 
+        style={{
+          objectFit: "fill",
+          width: "100%",
+          height: "35vh",
+          marginBottom: "15px"
+        }}
+        />
+      </div>
+
+      <div className="movie-info">
+        <img src={smallImageUrlBase + movie.backdrop_path} 
+          alt={movie.title}
+          style={{
+            objectFit: "fill",
+            width: "150px",
+            height: "250px",
+            marginRight: "15px"
+          }}
+        />
+
+        <div className="movie-details">
+          <h1 className="movie-title">{movie.title}</h1>
+          <p className="release-date">{movie.release_date}</p>
+          <p>{movie.vote_average.toFixed(1)} /10</p>
+        </div>
+      </div>
+
+        <div className="overview-container"> 
+          <p className="overview">{movie.overview}</p>
+        </div>
+      
     </div>
   )
 }
