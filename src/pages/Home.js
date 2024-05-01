82a09a5157ec5a93485ded7cf1a7dd826f7ae7c3
MattBarnabo <mattbarnabo@gmail.com>
2024-05-01 15:16:51 -0500

    adds banner image to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from "react"
 import HorrorMovies from "../components/HorrorMovies"
+import Banner from "../assets/index Red Banner.png"
 
 const Home = () => {
 	return (
 		<div className="page-body">
 			<div className="home-content">
 				<h1 className="home-header">Spooky Space Top Picks</h1>
+				<div className="home-banner-container">
+					<img
+						className="home-banner"
+						src={Banner}
+						alt=" red banner with ghost face with text reading whats your favorite movie"
+					/>
+				</div>
 				<HorrorMovies />
 			</div>
 		</div>
