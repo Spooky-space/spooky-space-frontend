0cf7e3bb4ffe1823bc99c2ab56a1247274ef0656
MattBarnabo <mattbarnabo@gmail.com>
2024-05-01 16:53:07 -0500

    added back in browserRouter and fixed return on home

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import "semantic-ui-css/semantic.min.css"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 )
 
