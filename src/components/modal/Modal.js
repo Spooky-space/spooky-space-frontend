e1f608cd83bcc200ff72703939c561256a36792d
mark19242 <mark19242@gmail.com>
2024-05-02 09:50:42 -0700

    fixed modals after merging issue

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
 
-// Now accepts a 'trigger' prop to customize the modal's trigger
 const Modals = ({ title, body, trigger, onAction, onCancel }) => {
   const [modal, setModal] = useState(false)
   const toggle = () => setModal(!modal)
@@ -13,30 +12,11 @@ const Modals = ({ title, body, trigger, onAction, onCancel }) => {
 
   return (
     <div>
-      {/* Use the provided trigger element */}
       {React.cloneElement(trigger, { onClick: toggle })}
       <Modal className="modal-content" isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>{title}</ModalHeader>
         <ModalBody>{body}</ModalBody>
-        <ModalFooter>
-          {/* <Button
-            onClick={() => {
-              onAction()
-              toggle()
-            }}
-          >
-            Do Something
-          </Button>{" "} */}
-          {/* <Button
-            className="submit-button"
-            onClick={() => {
-              onCancel()
-              toggle()
-            }}
-          >
-            Cancel
-          </Button> */}
-        </ModalFooter>
+        <ModalFooter></ModalFooter>
       </Modal>
     </div>
   )
