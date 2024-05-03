52b7139908f74571ea7a634e377f31c429dde1de
sethcrist <sethcrist50@gmail.com>
2024-05-03 12:10:22 -0700

    moved the genreList up and useHorrorMovies down

--- src/tmdbService.js
+++ src/tmdbService.js
@@ -1,59 +1,59 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
-import { apiConfig } from "./apiConfig.js"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { apiConfig } from "./apiConfig.js";
+
+const genreList = [
+	{ 28: "Action" },
+	{ 12: "Adventure" },
+	{ 16: "Animation" },
+	{ 35: "Comedy" },
+	{ 80: "Crime" },
+	{ 99: "Documentary" },
+	{ 18: "Drama" },
+	{ 10751: "Family" },
+	{ 14: "Fantasy" },
+	{ 36: "History" },
+	{ 27: "Horror" },
+	{ 10402: "Music" },
+	{ 9648: "Mystery" },
+	{ 10749: "Romance" },
+	{ 878: "Science_Fiction" },
+	{ 10770: "TV_Movie" },
+	{ 53: "Thriller" },
+	{ 10752: "War" },
+	{ 37: "Western" },
+];
 
 const useHorrorMovies = () => {
-	const [movies, setMovies] = useState([])
-	const [isLoading, setIsLoading] = useState(false)
-	const [error, setError] = useState(null)
-	const genreList = [
-		{ 28: "Action" },
-		{ 12: "Adventure" },
-		{ 16: "Animation" },
-		{ 35: "Comedy" },
-		{ 80: "Crime" },
-		{ 99: "Documentary" },
-		{ 18: "Drama" },
-		{ 10751: "Family" },
-		{ 14: "Fantasy" },
-		{ 36: "History" },
-		{ 27: "Horror" },
-		{ 10402: "Music" },
-		{ 9648: "Mystery" },
-		{ 10749: "Romance" },
-		{ 878: "Science_Fiction" },
-		{ 10770: "TV_Movie" },
-		{ 53: "Thriller" },
-		{ 10752: "War" },
-		{ 37: "Western" },
-	]
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		const fetchMovies = async () => {
-			setIsLoading(true)
+			setIsLoading(true);
 			try {
 				const response = await axios.get(`${apiConfig.baseURL}discover/movie`, {
 					params: {
 						api_key: apiConfig.api_key,
-						with_genres: "27,53", // Genre ID for horror, thriller
+						with_genres: "27,53", // Genre ID for horror and thriller
 					},
 					headers: apiConfig.headers,
-				})
-				setMovies(response.data.results)
-
-				setError(null)
+				});
+				setMovies(response.data.results);
+				setError(null);
 			} catch (err) {
-				setError(err)
-				setMovies([])
+				setError(err);
+				setMovies([]);
 			} finally {
-				setIsLoading(false)
+				setIsLoading(false);
 			}
-		}
+		};
 
-		fetchMovies()
-	}, [])
+		fetchMovies();
+	}, []);
 
-	return { movies, isLoading, error, genreList }
-}
+	return { movies, isLoading, error, genreList };
+};
 
-export default useHorrorMovies
+export default useHorrorMovies;
