c28fd6eaba96a45c37b683f4e32b99a41f192039
MattBarnabo <mattbarnabo@gmail.com>
2024-05-02 22:48:45 -0500

    removed commented out code

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,42 +5,6 @@ import NavButton from "./NavButton"
 import { NavLink } from "react-router-dom"
 import { Modal } from "reactstrap"
 
-// const Logo = styled.img`
-//   height: 150px;
-//   margin-top: -40px;
-//   object-fit: cover;
-//   position: fixed;
-// `
-
-// const NavLinks = styled.ul`
-//   list-style: none;
-//   display: flex;
-//   justify-content: end;
-//   margin: 0 0 0 auto;
-//   padding: 0;
-// `
-
-// const NavLinkItem = styled.li`
-//   padding: 20px 20px 0 20px;
-// `
-
-// const StyledNavLink = styled.a`
-//   background-color: red;
-//   color: white;
-//   text-decoration: none;
-//   font-family: "Creepster", cursive;
-//   font-size: 20px;
-//   padding: 10px 20px;
-//   border-radius: 5px;
-//   transition: background-color 0.3s;
-//   &:hover {
-//     background-color: darkred;
-//   }
-// `
-// const Spacer = styled.div`
-//   flex-grow: 1; // Takes up all available space
-// `
-
 const Header = () => {
 	return (
 		<>
