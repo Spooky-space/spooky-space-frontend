c2691f963582c82ab4c4fac73bbf20b29ce9b3a7
MattBarnabo <mattbarnabo@gmail.com>
2024-05-04 10:01:45 -0500

    adds connection of backend user and modal signup forms

--- src/components/modal/AboutUsModal.js
+++ src/components/modal/AboutUsModal.js
@@ -2,131 +2,133 @@ import React, { useState } from "react"
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
 import closeimage from "../modal/closeimage.png"
 
-const AboutUsModal = ({ trigger }) => {
-  const [modal, setModal] = useState(false)
-  const [nestedModals, setNestedModals] = useState({
-    matt: false,
-    seth: false,
-    mark: false,
-    amir: false,
-  })
+const AboutUsModal = ({ trigger, title }) => {
+	const [modal, setModal] = useState(false)
+	const [nestedModals, setNestedModals] = useState({
+		matt: false,
+		seth: false,
+		mark: false,
+		amir: false,
+	})
 
-  const toggle = () => setModal(!modal)
-  const toggleNested = (member) =>
-    setNestedModals((prev) => ({ ...prev, [member]: !prev[member] }))
+	const toggle = () => setModal(!modal)
+	const toggleNested = (member) =>
+		setNestedModals((prev) => ({ ...prev, [member]: !prev[member] }))
 
-  const teamDetails = {
-    matt: {
-      title: "Matt Barnabo",
-      content:
-        "Matt is the lead developer with extensive experience in full stack development...",
-    },
-    seth: {
-      title: "Seth Crist",
-      content:
-        "Seth specializes in backend technologies and cloud infrastructure...",
-    },
-    mark: {
-      title: "Mark Smith",
-      content:
-        "Mark is a frontend wizard, focusing on UX/UI and interactive designs...",
-    },
-    amir: {
-      title: "Amir Jackson",
-      content:
-        "Amir is our database expert with years of experience in SQL and NoSQL databases...",
-    },
-  }
+	const teamDetails = {
+		matt: {
+			title: "Matt Barnabo",
+			content:
+				"Matt is the lead developer with extensive experience in full stack development...",
+		},
+		seth: {
+			title: "Seth Crist",
+			content:
+				"Seth specializes in backend technologies and cloud infrastructure...",
+		},
+		mark: {
+			title: "Mark Smith",
+			content:
+				"Mark is a frontend wizard, focusing on UX/UI and interactive designs...",
+		},
+		amir: {
+			title: "Amir Jackson",
+			content:
+				"Amir is our database expert with years of experience in SQL and NoSQL databases...",
+		},
+	}
 
-  const TeamMemberModal = ({ member, isOpen, toggle }) => (
-    <Modal isOpen={isOpen} toggle={toggle}>
-      <ModalHeader>
-        {" "}
-        <h1 style={{ textShadow: "2px 2px 4px #FF0000", fontSize: "24px" }}>
-          {teamDetails[member].title}
-        </h1>{" "}
-      </ModalHeader>
-      <br></br>
-      <ModalBody>
-        <h1 style={{ textShadow: "2px 2px 4px #FF0000", fontSize: "24px" }}>
-          {teamDetails[member].content}
-        </h1>
-      </ModalBody>
-      <ModalFooter>
-        <Button className="submit-button" onClick={toggle}>
-          Close
-        </Button>
-      </ModalFooter>
-    </Modal>
-  )
+	const TeamMemberModal = ({ member, isOpen, toggle }) => (
+		<Modal isOpen={isOpen} toggle={toggle}>
+			<ModalHeader>
+				{" "}
+				<h1 style={{ textShadow: "2px 2px 4px #FF0000", fontSize: "24px" }}>
+					{teamDetails[member].title}
+				</h1>{" "}
+			</ModalHeader>
+			<br></br>
+			<ModalBody>
+				<h1 style={{ textShadow: "2px 2px 4px #FF0000", fontSize: "24px" }}>
+					{teamDetails[member].content}
+				</h1>
+			</ModalBody>
+			<ModalFooter>
+				<Button className="submit-button" onClick={toggle}>
+					Close
+				</Button>
+			</ModalFooter>
+		</Modal>
+	)
 
-  return (
-    <div>
-      {React.cloneElement(trigger, { onClick: toggle })}
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader>
-          <img
-            src={closeimage}
-            alt="Close"
-            onClick={toggle}
-            style={{ cursor: "pointer", width: "30px", height: "30px" }}
-            className="close-icon"
-          />{" "}
-          <h4 className="gradient-text">About Us</h4>
-        </ModalHeader>
-        <ModalBody>
-          <p style={{ textShadow: "2px 2px 4px #FF0000", fontSize: "3vh" }}>
-            General information about the company...
-          </p>
-          <Button
-            className="submit-button"
-            onClick={() => toggleNested("matt")}
-          >
-            About Matt
-          </Button>
-          <Button
-            className="submit-button"
-            onClick={() => toggleNested("seth")}
-          >
-            About Seth
-          </Button>
-          <Button
-            className="submit-button"
-            onClick={() => toggleNested("mark")}
-          >
-            About Mark
-          </Button>
-          <Button
-            className="submit-button"
-            onClick={() => toggleNested("amir")}
-          >
-            About Amir
-          </Button>
+	return (
+		<div>
+			<Button className="button" onClick={toggle}>
+				About Us
+			</Button>
+			<Modal isOpen={modal} toggle={toggle}>
+				<ModalHeader>
+					<img
+						src={closeimage}
+						alt="Close"
+						onClick={toggle}
+						style={{ cursor: "pointer", width: "30px", height: "30px" }}
+						className="close-icon"
+					/>{" "}
+					<h4 className="gradient-text">About Us</h4>
+				</ModalHeader>
+				<ModalBody>
+					<p style={{ textShadow: "2px 2px 4px #FF0000", fontSize: "3vh" }}>
+						General information about the company...
+					</p>
+					<Button
+						className="submit-button"
+						onClick={() => toggleNested("matt")}
+					>
+						About Matt
+					</Button>
+					<Button
+						className="submit-button"
+						onClick={() => toggleNested("seth")}
+					>
+						About Seth
+					</Button>
+					<Button
+						className="submit-button"
+						onClick={() => toggleNested("mark")}
+					>
+						About Mark
+					</Button>
+					<Button
+						className="submit-button"
+						onClick={() => toggleNested("amir")}
+					>
+						About Amir
+					</Button>
 
-          <TeamMemberModal
-            member="matt"
-            isOpen={nestedModals.matt}
-            toggle={() => toggleNested("matt")}
-          />
-          <TeamMemberModal
-            member="seth"
-            isOpen={nestedModals.seth}
-            toggle={() => toggleNested("seth")}
-          />
-          <TeamMemberModal
-            member="mark"
-            isOpen={nestedModals.mark}
-            toggle={() => toggleNested("mark")}
-          />
-          <TeamMemberModal
-            member="amir"
-            isOpen={nestedModals.amir}
-            toggle={() => toggleNested("amir")}
-          />
-        </ModalBody>
-      </Modal>
-    </div>
-  )
+					<TeamMemberModal
+						member="matt"
+						isOpen={nestedModals.matt}
+						toggle={() => toggleNested("matt")}
+					/>
+					<TeamMemberModal
+						member="seth"
+						isOpen={nestedModals.seth}
+						toggle={() => toggleNested("seth")}
+					/>
+					<TeamMemberModal
+						member="mark"
+						isOpen={nestedModals.mark}
+						toggle={() => toggleNested("mark")}
+					/>
+					<TeamMemberModal
+						member="amir"
+						isOpen={nestedModals.amir}
+						toggle={() => toggleNested("amir")}
+					/>
+				</ModalBody>
+			</Modal>
+		</div>
+	)
 }
 
 export default AboutUsModal
--- src/components/modal/SignUpForm.js
+++ src/components/modal/SignUpForm.js
@@ -1,106 +1,112 @@
 import React from "react"
 import { useForm } from "react-hook-form"
 import { Button, Form, FormGroup, Label, Input, FormFeedback } from "reactstrap"
+import { useNavigate } from "react-router-dom"
 
-const SignUpForm = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm()
+const SignUpForm = ({ signUp }) => {
+	const navigate = useNavigate()
+	const preloadedValues = {
+		username: "User4",
+		email: "test4@example.com",
+		password: "password",
+		password_confirmation: "password",
+	}
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm({ defaultValues: preloadedValues })
 
-  const onSubmit = (data) => {
-    console.log(data)
-    // Handle account creation logic here, typically sending data to an API
-  }
+	const onSubmit = (newUser) => {
+		signUp({ user: newUser })
+		navigate("/")
+	}
 
-  // Function to validate if both passwords match
-  const passwordsMatch = (value) => value === watch("password")
+	// Function to validate if both passwords match
+	const passwordsMatch = (value) => value === watch("password")
 
-  return (
-    <div className="form-container">
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup>
-          <br></br>
-          <br></br>
-          <br></br>
-          <Label for="username">Username </Label>
-          <Input
-            id="username"
-            name="username"
-            type="text"
-            className="form-control"
-            {...register("username", { required: "Username is required" })}
-            invalid={errors.username ? true : false}
-          />
-          {errors.username && (
-            <FormFeedback>{errors.username.message}</FormFeedback>
-          )}
-        </FormGroup>
-        <br></br>
-        <FormGroup>
-          <Label for="emailSignUp">Email</Label>
-          <Input
-            id="emailSignUp"
-            name="email"
-            type="email"
-            className="form-control"
-            {...register("email", { required: "Email is required" })}
-            invalid={errors.email ? true : false}
-          />
-          {errors.email && <FormFeedback>{errors.email.message}</FormFeedback>}
-        </FormGroup>
-        <br></br>
-        <FormGroup>
-          <Label for="passwordSignUp">Password</Label>
-          <Input
-            id="passwordSignUp"
-            name="password"
-            type="password"
-            className="form-control"
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 8,
-                message: "Password must be at least 8 characters",
-              },
-            })}
-            invalid={errors.password ? true : false}
-          />
-          {errors.password && (
-            <FormFeedback>{errors.password.message}</FormFeedback>
-          )}
-        </FormGroup>
-        <br></br>
-        <FormGroup>
-          <Label for="passwordConfirm">Confirm Password</Label>
-          <Input
-            id="passwordConfirm"
-            name="passwordConfirm"
-            type="password"
-            className="form-control"
-            {...register("passwordConfirm", {
-              required: "Please confirm password",
-              validate: passwordsMatch,
-            })}
-            invalid={errors.passwordConfirm ? true : false}
-          />
-          {errors.passwordConfirm &&
-            (errors.passwordConfirm.type === "validate" ? (
-              <FormFeedback>Passwords do not match</FormFeedback>
-            ) : (
-              <FormFeedback>{errors.passwordConfirm.message}</FormFeedback>
-            ))}
-        </FormGroup>
-        <div className="text-center">
-          <Button type="submit" className="nav-button create-account-submit">
-            Sign Up
-          </Button>
-        </div>
-      </Form>
-    </div>
-  )
+	return (
+		<div className="form-container">
+			<Form onSubmit={handleSubmit(onSubmit)}>
+				<FormGroup>
+					<br></br>
+					<br></br>
+					<br></br>
+					<Label for="username">Username </Label>
+					<input
+						id="username"
+						name="username"
+						type="text"
+						className="form-control"
+						{...register("username", { required: "Username is required" })}
+					/>
+					{errors.username && (
+						<FormFeedback>{errors.username.message}</FormFeedback>
+					)}
+				</FormGroup>
+				<br></br>
+				<FormGroup>
+					<Label for="email">Email</Label>
+					<input
+						id="email"
+						name="email"
+						type="email"
+						className="form-control"
+						{...register("email", { required: "Email is required" })}
+					/>
+					{errors.email && <FormFeedback>{errors.email.message}</FormFeedback>}
+				</FormGroup>
+				<br></br>
+				<FormGroup>
+					<Label for="password">Password</Label>
+					<input
+						id="password"
+						name="password"
+						type="password"
+						className="form-control"
+						{...register("password", {
+							required: "Password is required",
+							minLength: {
+								value: 8,
+								message: "Password must be at least 8 characters",
+							},
+						})}
+					/>
+					{errors.password && (
+						<FormFeedback>{errors.password.message}</FormFeedback>
+					)}
+				</FormGroup>
+				<br></br>
+				<FormGroup>
+					<Label for="password_confirmation">Confirm Password</Label>
+					<input
+						id="password_confirmation"
+						name="password_confirmation"
+						type="password"
+						className="form-control"
+						{...register("password_confirmation", {
+							required: "Please confirm password",
+							validate: passwordsMatch,
+						})}
+					/>
+					{errors.password_confirmation &&
+						(errors.password_confirmation.type === "validate" ? (
+							<FormFeedback>Passwords do not match</FormFeedback>
+						) : (
+							<FormFeedback>
+								{errors.password_confirmation.message}
+							</FormFeedback>
+						))}
+				</FormGroup>
+				<div className="text-center">
+					<Button type="submit" className="nav-button create-account-submit">
+						Sign Up
+					</Button>
+				</div>
+			</Form>
+		</div>
+	)
 }
 
 export default SignUpForm
