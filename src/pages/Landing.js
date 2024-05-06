c8f5780279795d95156544abda9125b7d5c35665
mark19242 <mark19242@gmail.com>
2024-05-06 08:42:48 -0700

    updated audio with button

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,18 +1,37 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import Image from "../assets/red-tree2.png"
 import Logo from "../assets/logo-screenshot-removebg.png"
 import Modals from "../components/modal/Modal"
 import LoginForm from "../components/modal/LoginForm"
 import SignUpForm from "../components/modal/SignUpForm"
 import AboutUsModal from "../components/modal/AboutUsModal"
+import HalloweenRain from "../assets/HalloweenRain.mp3"
+import playImage from "../assets/play.png"
+import pauseImage from "../assets/stop.png"
 
 const Landing = ({ signIn, signUp }) => {
   const handleAction = () => {}
-
   const handleCancel = () => {}
+  const [isPlaying, setIsPlaying] = useState(false)
+  const audioRef = useRef(null)
+
+  useEffect(() => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.play()
+      } else {
+        audioRef.current.pause()
+      }
+    }
+  }, [isPlaying])
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying)
+  }
 
   return (
     <div className="page-body landing-background">
+      <audio ref={audioRef} src={HalloweenRain} loop />
       <div className="landing-background-image-container">
         <img
           src={Image}
@@ -25,6 +44,13 @@ const Landing = ({ signIn, signUp }) => {
           <img src={Logo} alt="Spooky Space" className="landing-logo" />
         </div>
         <div className="landing-button-container">
+          <button onClick={togglePlay} className="audio-control-button">
+            <img
+              src={isPlaying ? pauseImage : playImage}
+              alt={isPlaying ? "Pause Music" : "Play Music"}
+              className="play-pause-icon"
+            />
+          </button>
           <Modals
             title="Create Account"
             body={<SignUpForm signUp={signUp} />}
